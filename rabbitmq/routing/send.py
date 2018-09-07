d6f11527915a0e196ff9890f11908b7fc2f90ec4
fuchenjie <709124637@qq.com>
2018-09-07 18:31:12 +0800

    dev_v1.0

--- rabbitmq/routing/send.py
+++ rabbitmq/routing/send.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
     connection=pika.BlockingConnection(pika.ConnectionParameters("58.87.97.238", 5672, "/",credentials))
     #创建管道
     channel=connection.channel()
-    #定义管道 direct topic主题类型 headers  fanout
+    #定义转换器 direct topic主题类型 headers  fanout
     channel.exchange_declare(exchange="direct_message",exchange_type="direct")
     #发送消息
     channel.basic_publish(exchange="direct_message",routing_key="type2",body="有选择的接收消息")
